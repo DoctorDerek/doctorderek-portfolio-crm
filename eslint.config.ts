a7191b709de81eb55763d4c2ea8111931afe162a
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:56:36 -0600

    chore(tooling): implement jiti and strongly type ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,8 @@
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
 import type { Linter } from "eslint"
 import gitignore from "eslint-config-flat-gitignore"
-import { dirname } from "path"
-import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)

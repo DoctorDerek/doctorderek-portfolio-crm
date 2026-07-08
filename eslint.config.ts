a4c014b0f8dd8d48640b8fce455f388c4bdc271e
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:51:24 -0600

    chore(tooling): migrate eslint config to typescript

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc"
+import type { Linter } from "eslint"
+import gitignore from "eslint-config-flat-gitignore"
 import { dirname } from "path"
 import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
-import gitignore from "eslint-config-flat-gitignore"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,7 +11,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
+const eslintConfig: Linter.Config[] = [
   gitignore(),
   ...compat.extends("next/core-web-vitals"),
   ...compat.extends("prettier"),

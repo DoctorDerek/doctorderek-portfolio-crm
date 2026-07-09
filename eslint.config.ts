7f6ba0dccf5c8c0fe11a9e903e0146e05bb3489b
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 18:42:33 -0600

    refactor(linting): temporarily disable typescript-eslint for TS 7 compatibility

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,9 +3,18 @@ import nextConfig from "eslint-config-next"
 import prettierConfig from "eslint-config-prettier"
 import onlyWarn from "eslint-plugin-only-warn"
 
+/**
+ * ONE-TIME EXCEPTION TO NO CODE COMMENT RULE:
+ * typescript-eslint (v8.63.0) is broken with TypeScript 7 (v7.0.2)
+ * TODO Restore typescript-eslint rules as soon as possible!!
+ * */
+const filteredNextConfig = nextConfig.filter(
+  (config) => config.name !== "next/typescript",
+)
+
 export default [
   gitignore(),
-  ...nextConfig,
+  ...filteredNextConfig,
   prettierConfig,
   {
     plugins: {
